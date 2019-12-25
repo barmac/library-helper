ca7d70826420f578e98cb50212c9ffa65f797676
Maciej Barelkowski <maciejbarel@gmail.com>
2019-12-25 14:43:50 +0100

    feat(browser): disable more features
    Done for memory and cpu optimization.

--- util/browser.js
+++ util/browser.js
@@ -1,10 +1,19 @@
 const puppeteer = require('puppeteer');
 
 module.exports = async function getBrowser() {
-  return puppeteer.launch({ args: [
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-    '--disable-dev-shm-usage',
-    '--single-process'
-  ] });
+  return puppeteer.launch({
+    headless: true,
+    args: [
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--disable-setuid-sandbox',
+      '--no-first-run',
+      '--no-sandbox',
+      '--no-zygote',
+      '--single-process',
+      '--proxy-server=\'direct://\'',
+      '--proxy-bypass-list=*',
+      '--deterministic-fetch',
+    ]
+  });
 }
