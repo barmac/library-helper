4b305d9f9b6189490f9d815124a7fb3804960a72
Maciej Barelkowski <maciejbarel@gmail.com>
2019-12-25 19:44:51 +0100

    feat(project): add logging

--- index.js
+++ index.js
@@ -12,17 +12,28 @@ const getEmailTemplate = require('./tasks/get-email-template');
 run();
 
 async function run() {
-  const browser = await getBrowser();
-
-  let page = await browser.newPage(),
-      loans;
+  let loans, browser;
 
   try {
+    console.log('Opening browser...');
+    browser = await getBrowser();
+
+    console.log('Opening new page...');
+    const page = await browser.newPage();
+
+    console.log('Signing in...');
     await signIn(page);
+
+    console.log('Renewing loans...');
     await renewLoans(page);
 
+    console.log('Getting pending loans...');
     loans = await getLoans(page);
+  } catch (error) {
+    console.error('Failed in the browser: ', error);
+    return;
   } finally {
+    console.log('Closing browser...');
     await browser.close();
   }
   const deadlineThreshold = new Date();
@@ -31,10 +42,17 @@ async function run() {
   const booksToReturnSoon = loans.filter(({ deadline }) => deadline < deadlineThreshold);
 
   if (!booksToReturnSoon.length) {
+    console.log('No books to return soon; skipping email phase.');
     return;
   }
 
-  const emailTemplate = getEmailTemplate(booksToReturnSoon);
+  try {
+    console.log('Getting email template...');
+    const emailTemplate = getEmailTemplate(booksToReturnSoon);
 
-  await sendEmail({ text: emailTemplate, subject: config.email.subject });
+    console.log('Sending email...');
+    await sendEmail({ text: emailTemplate, subject: config.email.subject });
+  } catch (error) {
+    console.error('Failed to send the email: ', error);
+  }
 }
